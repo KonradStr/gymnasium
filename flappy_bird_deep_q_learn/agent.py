eb1a189a0986952b1fdec8c88ca04992f473c680
Konrad <starzyk.konrad@gmail.com>
2025-05-15 19:51:48 +0200

    DQN

--- flappy_bird_deep_q_learn/agent.py
+++ flappy_bird_deep_q_learn/agent.py
@@ -1,30 +1,210 @@
+import itertools
+import random
 import flappy_bird_gymnasium
 import gymnasium
+import numpy as np
+from torch import nn
+from tensorflow.python.ops.metrics_impl import mean_per_class_accuracy
+
 from dqn import DQN
 import torch
+from experience_replay import ReplayMemory
+import yaml
+import os
+import matplotlib.pyplot as plt
+import matplotlib
+
+from datetime import datetime, timedelta
+
+
+DATE_FORMAT = "%m-%d %H:%M:%S"
+RUNS_DIR = "runs"
+os.makedirs(RUNS_DIR, exist_ok=True)
+
+matplotlib.use('Agg')
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+
 class Agent:
+    def __init__(self, hyperparameter_set):
+        with open('hyperparameters.yml', 'r') as file:
+            all_hyperparameter_sets = yaml.safe_load(file)
+            hyperparameters = all_hyperparameter_sets[hyperparameter_set]
+
+        self.hyperparameters_set = hyperparameter_set
+
+        self.env_id = hyperparameters['env_id']
+        self.learning_rate_a = hyperparameters['learning_rate_a']
+        self.discount_factor_g = hyperparameters['discount_factor_g']
+        self.network_sync_rate = hyperparameters['network_sync_rate']
+        self.replay_memory_size = hyperparameters['replay_memory_size']
+        self.mini_batch_size = hyperparameters['mini_batch_size']
+        self.epsilon_init = hyperparameters['epsilon_init']  # 1 - 100% random action
+        self.epsilon_decay = hyperparameters['epsilon_decay']  # decay rate
+        self.epsilon_min = hyperparameters['epsilon_min']  # minimum value
+        self.stop_on_reward = hyperparameters['stop_on_reward']
+        self.fc1_nodes = hyperparameters['fc1_nodes']
+        self.env_make_params = hyperparameters.get('env_make_params', {}) # optional -> default empty dict
+
+
+        self.loss_fn = nn.MSELoss() #MSE = Mean Square Error
+        self.optimizer = None
+
+        self.LOG_FILE = os.path.join(RUNS_DIR, f'{self.hyperparameters_set}.log')
+        self.MODEL_FILE = os.path.join(RUNS_DIR, f'{self.hyperparameters_set}.pt')
+        self.GRAPH_FILE = os.path.join(RUNS_DIR, f'{self.hyperparameters_set}.png')
+
     def run(self, is_training=True, render=False):
-        env = gymnasium.make("FlappyBird-v0", render_mode="human" if render else None)
+        if is_training:
+            start_time = datetime.now()
+            last_graph_update_time = start_time
+            log_message = f"{start_time.strftime(DATE_FORMAT)}: Training starting..."
+            print(log_message)
+            with open(self.LOG_FILE, 'w') as file:
+                file.write(log_message + '\n')
+
+        env = gymnasium.make(self.env_id, render_mode="human" if render else None, **self.env_make_params)
 
         num_states = env.observation_space.shape[0]
         num_actions = env.action_space.n
 
-        policy_dqn = DQN(num_states, num_actions).to_device(device)
+        rewards_per_episode = []
 
-        obs, _ = env.reset()
-        while True:
-            # Next action:
-            # (feed the observation to your agent here)
-            action = env.action_space.sample()
 
-            # Processing:
-            obs, reward, terminated, _, info = env.step(action)
+        policy_dqn = DQN(num_states, num_actions, self.fc1_nodes).to(device)
 
-            # Checking if the player is still alive
-            if terminated:
-                break
+        if is_training:
+            memory = ReplayMemory(self.replay_memory_size)
+            epsilon = self.epsilon_init
 
-        env.close()
+            target_dqn = DQN(num_states, num_actions, self.fc1_nodes).to(device)
+            target_dqn.load_state_dict(policy_dqn.state_dict())
+
+            step_count = 0
+
+            self.optimizer = torch.optim.Adam(policy_dqn.parameters(), lr=self.learning_rate_a)
+
+            epsilon_history = []
+
+            best_reward = -9999999
+        else:
+            policy_dqn.load_state_dict(torch.load(self.MODEL_FILE)) # load learned policy
+            policy_dqn.eval()
+
+
+        for episode in itertools.count():
+            state, _ = env.reset()
+            state = torch.tensor(state, dtype=torch.float, device=device)
+            terminated = False
+
+            episode_reward = 0.0
+
+            while (not terminated and episode_reward < self.stop_on_reward ):
+                if is_training and random.random() < epsilon:
+                    action = env.action_space.sample()
+                    action = torch.tensor(action, dtype=torch.int64, device=device)
+                else:
+                    with torch.no_grad():
+                        action = policy_dqn(state.unsqueeze(dim=0)).squeeze().argmax()
+
+                new_state, reward, terminated, truncated, info = env.step(action.item())
+
+                episode_reward += reward
+
+                # convert new_state and reward to tensors on device
+                new_state = torch.tensor(new_state, dtype=torch.float, device=device)
+                reward = torch.tensor(reward, dtype=torch.float, device=device)
+
+                if is_training:
+                    memory.append((state, action, new_state, reward, terminated))
+                    step_count += 1
+
+                state = new_state
+
+            rewards_per_episode.append(episode_reward)
+
+            if is_training:
+                if episode_reward > best_reward:
+                    log_message = f"{datetime.now().strftime(DATE_FORMAT)}: New best reward {episode_reward:0.1f} ({(episode_reward-best_reward)/best_reward*100:+.1f}%) at episode {episode}, saving model..."
+                    print(log_message)
+                    with open(self.LOG_FILE, 'a') as file:
+                        file.write(log_message + '\n')
+                    torch.save(policy_dqn.state_dict(), self.MODEL_FILE)
+                    best_reward = episode_reward
+
+                current_time = datetime.now()
+                if current_time - last_graph_update_time > timedelta(seconds=10):
+                    self.save_graph(rewards_per_episode, epsilon_history)
+                    last_graph_update_time = current_time
+
+                # if enough experience has been collected
+                if len(memory) > self.mini_batch_size:
+                    mini_batch = memory.sample(self.mini_batch_size)
+                    self.optimize(mini_batch, policy_dqn, target_dqn)
+
+                    epsilon = max(epsilon * self.epsilon_decay, self.epsilon_min)
+                    epsilon_history.append(epsilon)
+
+                    if step_count > self.network_sync_rate:
+                        target_dqn.load_state_dict(policy_dqn.state_dict())
+                        step_count = 0
+
+
+
+            # if len(memory) > self.mini_batch_size:
+            #     mini_batch = memory.sample(self.mini_batch_size)
+            #     self.optimize(mini_batch, policy_dqn, target_dqn)
+        # env.close()
+
+    def save_graph(self, rewards_per_episode, epsilon_history):
+        fig = plt.figure(1)
+
+        mean_rewards = np.zeros(len(rewards_per_episode))
+        for x in range(len(mean_rewards)):
+            mean_rewards[x] = np.mean(rewards_per_episode[max(0, x-99):(x+1)])
+        plt.subplot(121) #1row x 2col grid at cell 1
+        plt.ylabel('Mean Rewards')
+        plt.plot(mean_rewards)
+
+        plt.subplot(122)
+        plt.ylabel('Episode decay')
+        plt.plot(epsilon_history)
+
+        plt.subplots_adjust(wspace=1.0, hspace=0.5)
+        plt.savefig(self.GRAPH_FILE)
+        plt.close()
+
+
+
+    def optimize(self, mini_batch, policy_dqn, target_dqn):
+        states, actions, new_states, rewards, terminations = zip(*mini_batch)
+
+        states = torch.stack(states)
+        actions = torch.stack(actions)
+        new_states = torch.stack(new_states)
+        rewards = torch.stack(rewards)
+        terminations = torch.tensor(terminations).float().to(device)
+
+        with torch.no_grad():
+            target_q = rewards + (1- terminations) * self.discount_factor_g * target_dqn(new_states).max(dim=1)[0]
+
+
+
+        current_q = policy_dqn(states).gather(dim=1, index=actions.unsqueeze(dim=1)).squeeze()
+
+
+
+        loss = self.loss_fn(current_q, target_q)
+
+        self.optimizer.zero_grad()  # clear gradients
+        loss.backward()             # computer gradients
+        self.optimizer.step()       # update network parameters i.e. weights and biases
+
+if __name__ == '__main__':
+    # agent = Agent("cartpole1")
+    agent = Agent("flappybird1")
+    # agent = Agent("lunarlander1")
+    # agent = Agent("carracing1")
+    # agent.run(is_training=True, render=False)
+    agent.run(is_training=False, render=True)
--- flappy_bird_deep_q_learn/dqn.py
+++ flappy_bird_deep_q_learn/dqn.py
@@ -7,17 +7,23 @@ class DQN(nn.Module):
     def __init__(self, state_dim, action_dim, hidden_dim=256):
         super(DQN, self).__init__()
         self.fc1 = nn.Linear(state_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, action_dim) # output layer
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, action_dim) # output layer
 
     # x is state (12 information values) -> sending state through layer 1
     def forward(self, x):
         x = F.relu(self.fc1(x)) # activation function?
-        return self.fc2(x)
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
 
-if __name__ == '__main__':
-    state_dim = 12
-    action_dim = 2
-    net = DQN(state_dim, action_dim)
-    state = torch.rand(1, state_dim)
-    output = net(state)
-    print(output)
+# class DQN(nn.Module):
+#     # __init__ Defines the layers
+#     def __init__(self, state_dim, action_dim, hidden_dim=256):
+#         super(DQN, self).__init__()
+#         self.fc1 = nn.Linear(state_dim, hidden_dim)
+#         self.fc2 = nn.Linear(hidden_dim, action_dim)  # output layer
+#
+#     # x is state (12 information values) -> sending state through layer 1
+#     def forward(self, x):
+#         x = F.relu(self.fc1(x))  # activation function?
+#         return self.fc2(x)
